0766003759c2a714ddb43f0664a9d944350fd38e
mrben997 <ngduth97@gmail.com>
2024-05-09 14:20:01 +0700

    fix: code build - call api wrong

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,20 +5,18 @@ import Content from './_component/Content'
 
 interface IProps {}
 
-const getData = async () => {
-  try {
-    const res = await global.fetch('http://localhost:3000/api/news')
-    if (!res.ok) return []
-    const data = await res.json()
-    return data
-  } catch (error) {
-    return []
-  }
-}
-
 const HomePage: FC<IProps> = async (props: IProps) => {
+  const getData = async () => {
+    try {
+      const res = await global.fetch('http://localhost:3000/api/news')
+      if (!res.ok) return []
+      const data = await res.json()
+      return data
+    } catch (error) {
+      return []
+    }
+  }
   const data = await getData()
-
   return (
     <Container style={{ padding: '18px 0 56px' }}>
       <Content data={data} />
