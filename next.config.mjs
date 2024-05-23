0e93edee25999f7d41e31c5ca16d3a964b98f469
mrben997 <ngduth97@gmail.com>
2024-05-23 14:04:04 +0700

    fix: env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  env: {
-    API_URI: 'http://localhost:3000/api',
-    IMAGE_API_URI: 'http://localhost:3000/api/images'
-  }
+  output: 'standalone'
 }
 
 export default nextConfig
