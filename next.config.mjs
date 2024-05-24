91179315d0784170cbcbd967b69562ffc2ce4481
mrben997 <ngduth97@gmail.com>
2024-05-24 09:41:46 +0700

    feat: add adsense - 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_URI: 'http://localhost:3000/api',
-    IMAGE_API_URI: 'http://localhost:3000/api/images'
-  }
+  // env: {
+  //   API_URI: 'http://localhost:3000/api',
+  //   IMAGE_API_URI: 'http://localhost:3000/api/images'
+  // },
 }
 
 export default nextConfig
